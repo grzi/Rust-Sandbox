55d9d8ff41a4261ccf44bbfc918b54091ecf646c
Jérémy Thulliez <jeremy.thulliez@zenika.com>
2018-11-22 23:08:25 +0100

    feat(game_is_ended) : added the impl of game_is_ended

--- 1.Samples/connect_4/src/main.rs
+++ 1.Samples/connect_4/src/main.rs
@@ -16,6 +16,7 @@ fn connect_4() {
     let mut board = [[' '; 7]; 6];
     let mut player = 'X';
 
+
     display_board(&board);
     loop {
         let mut choice = String::new();
@@ -33,23 +34,30 @@ fn connect_4() {
             }
         };
 
-        if add_piece(&mut board, choice - 1, &player) {
+        if add_piece(&mut board, choice, &player) {
             display_board(&board);
-            if game_ended(&board) {
-                break;
-            } else {
-                change_player(&mut player);
+            match game_ended(&board, &player) {
+                0 => {
+                    println!("Game end : Equality");
+                    break;
+                },
+                1 => {
+                    println!("Congrats, player {} win !", &player);
+                    break;
+                },
+                _ => {
+                    change_player(&mut player);
+                }
             }
         } else {
             println!("Wrong choice");
             continue;
         }
     }
-    println!("Congrats, player {} win !", &player);
 }
 
 fn display_board(_board: &[[char; 7]; 6]) {
-    println!("  1  2  3  4  5  6  7");
+    println!("  0  1  2  3  4  5  6");
     println!("  -  -  -  -  -  -  -");
     for lines in _board.iter() {
         print!("|");
@@ -61,9 +69,8 @@ fn display_board(_board: &[[char; 7]; 6]) {
     println!("  -  -  -  -  -  -  -");
 }
 
-//TODO : implement this
 fn add_piece(_board: &mut [[char; 7]; 6], _choice: usize, _player: &char) -> bool {
-    if _choice > 0 && _choice < 8 && _board[0][_choice] == ' ' {
+    if _choice < 8 && _board[0][_choice] == ' ' {
         for i in (0..6).rev() {
             if _board[i][_choice] == ' ' {
                 _board[i][_choice] = *_player;
@@ -76,9 +83,73 @@ fn add_piece(_board: &mut [[char; 7]; 6], _choice: usize, _player: &char) -> boo
     }
 }
 
-//TODO : implement this
-fn game_ended(_board: &[[char; 7]; 6]) -> bool {
+fn game_ended(_board: &[[char; 7]; 6], _player: &char) -> u32 {
+    if vertical_win(&_board, _player) || horizontal_win(&_board, _player)
+        || diagonnal_top(&_board, _player) || diagonnal_bottom(&_board, _player) {
+        return 1; // Game ended with a win
+    }
+
+    if board_is_full(_board) {
+        return 0; // Board is full
+    }
+
+    return 2; // Game continue
+}
+
+fn vertical_win(_board: &[[char; 7]; 6], _player: &char) -> bool {
+    for i in 0..5 {
+        let mut count = 0;
+        for j in 0..6 {
+            count = if _board[j][i] == *_player { count + 1 } else { 0 };
+            if count == 4 {
+                return true;
+            }
+        }
+    }
     false
+}
+
+fn horizontal_win(_board: &[[char; 7]; 6], _player: &char) -> bool {
+    for i in 0..6 {
+        let mut count = 0;
+        for j in 0..5 {
+            count = if _board[i][j] == *_player { count + 1 } else { 0 };
+            if count == 4 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn diagonnal_top(_board: &[[char; 7]; 6], _player: &char) -> bool {
+    for i in 3..6 {
+        for j in 0..4 {
+            if _board[i][j] == *_player && _board[i - 1][j + 1] == *_player
+                && _board[i - 2][j + 2] == *_player && _board[i - 3][j + 3] == *_player
+                { return true; }
+        }
+    }
+    false
+}
+
+fn diagonnal_bottom(_board: &[[char; 7]; 6], _player: &char) -> bool {
+    for i in 3..6 {
+        for j in 3..7 {
+            if _board[i][j] == *_player && _board[i - 1][j - 1] == *_player
+                && _board[i - 2][j - 2] == *_player && _board[i - 3][j - 3] == *_player
+                { return true; }
+        }
+    }
+    false
+}
+
+fn board_is_full(_board: &[[char; 7]; 6]) -> bool {
+    let mut count = 0;
+    for i in (0..7).rev() {
+        if _board[0][i] != ' ' { count = count + 1; }
+    }
+    return count == 7;
 }
 
 fn change_player(_player: &mut char) {
