f03df575a4ba3b7aa4731faf77869ffecb1dcd41
Jérémy Thulliez <jeremy.thulliez@zenika.com>
2018-12-08 11:44:54 +0100

    Advent of code 2018 - Optimisation day 8

--- 2.Advent_of_code_2018/src/day_8/mod.rs
+++ 2.Advent_of_code_2018/src/day_8/mod.rs
@@ -1,58 +1,32 @@
 use std::collections::VecDeque;
 use std::borrow::BorrowMut;
 
-pub fn execute(_line: String) -> (u32, u32) {
-    let mut queue = read_input(_line.lines().next().unwrap());
-    let tree = read_next_node(&mut queue);
+pub fn execute(_line: String) -> (usize, usize) {
+    let tree = TreeNode::read_next_node(&mut read_input(_line.lines().next().unwrap()));
     (get_total_metadata(&tree), get_total_metadata_by_child(&tree))
 }
 
-fn read_input(_input: &str) -> VecDeque<u32> {
-    let splitted = _input.split(" ").collect::<Vec<&str>>();
-    splitted.iter().map(|e| e.parse().unwrap()).collect()
+fn read_input(_input: &str) -> VecDeque<usize> {
+    _input.split(" ").collect::<Vec<&str>>().iter().map(|e| e.parse().unwrap()).collect()
 }
 
-fn read_next_node(_queue: &mut VecDeque<u32>) -> TreeNode {
-    let mut this_node = TreeNode::new();
-
-    let child_number = _queue.pop_front().unwrap();
-    let metadata_number = _queue.pop_front().unwrap();
-
-    for i in 0..child_number {
-        this_node.children.push(read_next_node(_queue));
-    }
-
-    for i in 0..metadata_number {
-        this_node.metadata.push(_queue.pop_front().unwrap());
-    }
-
-    this_node
+fn get_total_metadata(_tree: &TreeNode) -> usize {
+    _tree.metadata.iter().sum::<usize>()
+        + _tree.children.iter().map(|e| get_total_metadata(&e)).sum::<usize>()
 }
 
-fn get_total_metadata(_tree: &TreeNode) -> u32 {
-    _tree.metadata.iter().sum::<u32>()
-        + _tree.children.iter().map(|e| get_total_metadata(&e)).sum::<u32>()
-}
-
-fn get_total_metadata_by_child(_tree: &TreeNode) -> u32 {
-    let mut res = 0;
-
-    if _tree.children.len() > 0 {
-        for metadata in &_tree.metadata {
-            if metadata - 1 < (_tree.children.len() as u32) {
-                res += get_total_metadata_by_child(&_tree.children.get((metadata - 1) as usize).unwrap())
-            }
-        }
-    } else {
-        res += _tree.metadata.iter().sum::<u32>();
+fn get_total_metadata_by_child(_tree: &TreeNode) -> usize {
+    if _tree.children.len() == 0 {
+        return _tree.metadata.iter().sum::<usize>();
     }
 
-    res
+    _tree.metadata.iter().filter(|e| (**e - 1 ) < _tree.children.len())
+        .map(|e| get_total_metadata_by_child(_tree.children.get(*e - 1).unwrap())).sum()
 }
 
 struct TreeNode {
     children: Vec<TreeNode>,
-    metadata: Vec<u32>,
+    metadata: Vec<usize>,
 }
 
 impl TreeNode {
@@ -61,6 +35,23 @@ impl TreeNode {
             children: Vec::new(),
             metadata: Vec::new(),
         }
+    }
+
+    fn read_next_node(_queue: &mut VecDeque<usize>) -> TreeNode {
+        let mut this_node = TreeNode::new();
+
+        let child_number = _queue.pop_front().unwrap();
+        let metadata_number = _queue.pop_front().unwrap();
+
+        for i in 0..child_number {
+            this_node.children.push(TreeNode::read_next_node(_queue));
+        }
+
+        for i in 0..metadata_number {
+            this_node.metadata.push(_queue.pop_front().unwrap());
+        }
+
+        this_node
     }
 }
 
