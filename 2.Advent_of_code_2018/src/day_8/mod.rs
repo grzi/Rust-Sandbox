b2b03d7c90691d5abd5a149a8de8a0a3ad17d010
Jérémy Thulliez <jeremy.thulliez@zenika.com>
2018-12-08 10:38:21 +0100

    Advent Of Code - day 8

---
+++ 2.Advent_of_code_2018/src/day_8/mod.rs
@@ -0,0 +1,91 @@
+use std::collections::VecDeque;
+use std::borrow::BorrowMut;
+
+pub fn execute(_line: String) -> (u32, u32) {
+    let mut queue = read_input(_line.lines().next().unwrap());
+    let tree = read_next_node(&mut queue);
+    (get_total_metadata(&tree), get_total_metadata_by_child(&tree))
+}
+
+fn read_input(_input: &str) -> VecDeque<u32> {
+    let splitted = _input.split(" ").collect::<Vec<&str>>();
+    splitted.iter().map(|e| e.parse().unwrap()).collect()
+}
+
+fn read_next_node(_queue: &mut VecDeque<u32>) -> TreeNode {
+    let mut this_node = TreeNode::new();
+
+    this_node.child_number = _queue.pop_front().unwrap();
+    this_node.metadata_number = _queue.pop_front().unwrap();
+
+    for i in 0..this_node.child_number {
+        this_node.children.push(read_next_node(_queue));
+    }
+
+    for i in 0..this_node.metadata_number {
+        this_node.metadata.push(_queue.pop_front().unwrap());
+    }
+
+    this_node
+}
+
+fn get_total_metadata(_tree: &TreeNode) -> u32 {
+    let mut res = 0;
+    for metadata in &_tree.metadata {
+        res += metadata
+    }
+
+    for child in &_tree.children {
+        res += get_total_metadata(&child);
+    }
+
+    res
+}
+
+fn get_total_metadata_by_child(_tree: &TreeNode) -> u32 {
+    let mut res = 0;
+
+    if _tree.children.len() > 0 {
+        for metadata in &_tree.metadata {
+            if metadata - 1 < *&_tree.children.len() as u32 { // TODO : Enquêter sur cette synthaxe douteuse
+                res += get_total_metadata_by_child(&_tree.children.get((metadata - 1) as usize).unwrap())
+            }
+        }
+    } else {
+        for metadata in &_tree.metadata {
+            res += metadata
+        }
+    }
+
+    res
+}
+
+
+struct TreeNode {
+    child_number: u32,
+    children: Vec<TreeNode>,
+    metadata_number: u32,
+    metadata: Vec<u32>,
+}
+
+impl TreeNode {
+    fn new() -> TreeNode {
+        TreeNode {
+            child_number: 0,
+            children: Vec::new(),
+            metadata_number: 0,
+            metadata: Vec::new(),
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_input() {
+        assert_eq!(read_input("1 2 3 4 5"), [1, 2, 3, 4, 5]);
+    }
+}
--- 2.Advent_of_code_2018/src/main.rs
+++ 2.Advent_of_code_2018/src/main.rs
@@ -12,10 +12,11 @@ mod day_4;
 mod day_5;
 mod day_6;
 mod day_7;
+mod day_8;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let response = day_7::execute(read_input_file(&args[1]));
+    let response = day_8::execute(read_input_file(&args[1]));
     println!("result : {}, {}", response.0, response.1);
 }
 
